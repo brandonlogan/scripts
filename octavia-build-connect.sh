7d8814d2180e10c96773d7e60dfdbd4bb4671b4c
Brandon Logan <brandon.logan@rackspace.com>
2015-09-04 04:37:22 -0500

    Cleanup of octavia_build_connect script

--- octavia-build-connect.sh
+++ octavia-build-connect.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 neutron="supernova neutron-ds-demo"
 nova="supernova nova-ds-demo"
-function setup_neutron_resources {
-    $neutron net-create user-net
-    $neutron subnet-create --name user-subnet user-net 10.2.2.0/24
+
+function add_security_group_rules {
     $neutron security-group-rule-create default --protocol icmp
     $neutron security-group-rule-create default --protocol tcp --port-range-min 22 --port-range-max 22
     $neutron security-group-rule-create default --protocol tcp --port-range-min 80 --port-range-max 80
 }
 
-function create_member {
-    $nova boot --image $($nova image-list | awk '/cirros/ {print $2}') --flavor 1 --nic net-id=$($nova net-list | awk '/user-net/ {print $2}') member1
-    status=$($nova show member1 | awk '/status/ {print $4}')
+function create_network_and_subnet {
+    net_name=$1
+    subnet_name=$2
+    cidr=$3
+    $neutron net-create ${net_name}
+    $neutron subnet-create --name ${subnet_name} ${net_name} ${cidr}
+}
+
+function get_server_ip {
+    name=$1
+    net_name=$2
+    member_ip=$($nova show ${name} | awk "/${net_name} "'network/ {print $5}')
+    echo $member_ip
+}
+
+function boot_server {
+    name=$1
+    net_name=$2
+    $nova boot --image $($nova image-list | awk '/cirros/ {print $2}') --flavor 1 --nic net-id=$($nova net-list | awk "/${net_name}/ "'{print $2}') ${name}
+    status=$($nova show ${name} | awk '/status/ {print $4}')
     while [ "$status" != "ACTIVE" ]
     do
         sleep 2
-        status=$($nova show member1 | awk '/status/ {print $4}')
+        status=$($nova show ${name} | awk '/status/ {print $4}')
     done
 }
-function start_member_web_service {
-    ip=$($nova show member1 | awk '/user-net network/ {print $5}')
-    net_id=$($neutron net-show user-net | awk '/ id / {print $4}')
+
+function start_web_service {
+    name=$1
+    net_name=$2
+    ip=$($nova show ${name} | awk "/${net_name}"' network/ {print $5}')
+    net_id=$($neutron net-show ${net_name} | awk '/ id / {print $4}')
     ns_name="qdhcp-$net_id"
-    SERVE_TRAFFIC="while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo -e 'member1'; } | sudo nc -l -p 80; done &"
-    #SERVE_TRAFFIC="ls"
+    SERVE_TRAFFIC="while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo -e '${name}'; } | sudo nc -l -p 80; done &"
     echo "sudo ip netns exec $ns_name sshpass -p cubswin:\) ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -oCheckHostIP=no cirros@$ip"
     sudo ip netns exec $ns_name sshpass -p cubswin:\) ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -oCheckHostIP=no cirros@$ip $SERVE_TRAFFIC
     while [ $? -ne 0 ]
@@ -33,6 +51,7 @@ function start_member_web_service {
         sudo ip netns exec $ns_name sshpass -p cubswin:\) ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -oCheckHostIP=no cirros@$ip $SERVE_TRAFFIC
     done
 }
+
 function wait_for_lb_active {
     echo "Waiting for $1 to become ACTIVE..."
     status=$($neutron lbaas-loadbalancer-show $1 | awk '/provisioning_status/ {print $4}')
@@ -47,24 +66,63 @@ function wait_for_lb_active {
         fi
      done
 }
-setup_neutron_resources
-create_member
-start_member_web_service
-ip=$($nova show member1 | awk '/user-net network/ {print $5}')
-$neutron lbaas-loadbalancer-create --name lb1 private-subnet
-wait_for_lb_active "lb1"
-$neutron lbaas-listener-create --name listener1 --loadbalancer lb1 --protocol HTTP --protocol-port 80
-wait_for_lb_active "lb1"
-$neutron lbaas-pool-create --name pool1 --listener listener1 --protocol HTTP --lb-algorithm ROUND_ROBIN
-wait_for_lb_active "lb1"
-$neutron lbaas-member-create --address $ip --subnet user-subnet --protocol-port 80 pool1
-wait_for_lb_active "lb1"
-vip=$($neutron lbaas-loadbalancer-show lb1 | awk '/vip_address/ {print $4}')
-echo "Testing connection to $vip"
-output=$(curl $vip 2>/dev/null)
-if [ $output == "member1" ]
-then
-    echo "Successful Connection!"
-else
-    echo "Connection Failed!"
-fi
+
+function get_vip {
+    vip=$($neutron lbaas-loadbalancer-show lb1 | awk '/vip_address/ {print $4}')
+    echo $vip
+}
+
+function create_lb {
+    vip_subnet=$1
+    $neutron lbaas-loadbalancer-create --name lb1 ${vip_subnet}
+    wait_for_lb_active "lb1"
+    $neutron lbaas-listener-create --name listener1 --loadbalancer lb1 --protocol HTTP --protocol-port 80
+    wait_for_lb_active "lb1"
+    $neutron lbaas-pool-create --name pool1 --listener listener1 --protocol HTTP --lb-algorithm ROUND_ROBIN
+    wait_for_lb_active "lb1"
+}
+
+function add_member {
+    ip=$1
+    subnet=$2
+    $neutron lbaas-member-create --address ${ip} --subnet ${subnet} --protocol-port 80 pool1
+}
+
+function create_network_and_member {
+    net_name=$1
+    subnet_name=$2
+    cidr=$3
+    member_name=$4
+    create_network_and_subnet ${net_name} ${subnet_name} ${cidr}
+    boot_server ${member_name} ${net_name}
+    member_ip=$(get_server_ip ${member_name} ${net_name})
+    start_web_service ${member_name} ${net_name}
+    add_member ${member_ip} ${subnet_name}
+    wait_for_lb_active "lb1"
+}
+
+function validate_connection {
+    vip=$1
+    member_name=$2
+    echo "Testing connection to $vip"
+    output=$(curl $vip 2>/dev/null)
+    if [ $output = ${member_name} ]
+    then
+        echo "Successful Connection!"
+    else
+        echo "Connection Failed!"
+    fi
+}
+
+add_security_group_rules
+create_lb "private-subnet"
+vip=$(get_vip)
+
+net_name="user-net"
+member_name="member1"
+subnet_name="user-subnet"
+cidr="10.2.2.0/24"
+create_network_and_member ${net_name} ${subnet_name} ${cidr} ${member_name}
+
+validate_connection ${vip} ${member_name}
+
