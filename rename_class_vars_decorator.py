5a0b64481ff1c41889222578229af4a6580c07ad
Brandon Logan <brandon.logan@rackspace.com>
2016-11-30 17:32:26 -0600

    Added script and decorator to rename variables of a class

---
+++ rename_class_vars_decorator.py
@@ -0,0 +1,43 @@
+def rename_kwargs(**renamed_kwargs):
+    def wrap(cls):
+        def __getattr__(instance, name):
+            if name in renamed_kwargs:
+               return getattr(instance, renamed_kwargs[name])
+            return getattr(instance, name)
+
+        def __setattr__(instance, name, value):
+            if name in renamed_kwargs:
+               instance.__dict__[renamed_kwargs[name]] = value
+            else:
+               instance.__dict__[name] = value
+
+        def wrapped_cls(*args, **kwargs):
+            for old_kwarg, new_kwarg in renamed_kwargs.items():
+                if old_kwarg in kwargs:
+                   kwargs[new_kwarg] = kwargs[old_kwarg]
+                   del kwargs[old_kwarg]
+            cls.__getattr__ = __getattr__
+            cls.__setattr__ = __setattr__
+            return cls(*args, **kwargs)
+        return wrapped_cls
+    return wrap
+
+
+@rename_kwargs(a='z')
+class C(object):
+    def __init__(self, x, y, z=None):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+if __name__ == '__main__':
+    x = C(1,2, a=3)
+    print("x.a = %d" % x.a)
+    print("x.z = %d" % x.z)
+    print("x.x = %d" % x.x)
+    print("Setting x.a = 4")
+    x.a = 4
+    print("x.a = %d" % x.a)
+    print("x.z = %d" % x.z)
+    print("x.x = %d" % x.x)
